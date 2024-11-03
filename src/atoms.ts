579e47be4d6deb176d37ebcd4a34477fea3278b9
MARUNGs <mi1764@naver.com>
2024-11-03 23:05:37 +0900

    round, goal 카운트 기능

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,13 +1,34 @@
 import { atom, selector } from "recoil";
 
-export const hourState = atom<number>({
+const hourState = atom<number>({
   key: "hour",
-  default: 25,
+  default: 0,
 });
 
-export const minuteState = atom<number>({
+const minuteState = atom<number>({
   key: "minute",
   default: 0,
+});
+
+interface IPoint {
+  current: number;
+  target: number;
+}
+
+const roundPoint = atom<IPoint>({
+  key: "rountPoint",
+  default: {
+    current: 0,
+    target: 4,
+  },
+});
+
+const goalPoint = atom<IPoint>({
+  key: "goalPoint",
+  default: {
+    current: 0,
+    target: 12,
+  },
 });
 
 export const hourSelector = selector<number>({
@@ -23,11 +44,22 @@ export const hourSelector = selector<number>({
 
 export const minuteSelector = selector<number>({
   key: "minuteSelector",
-  get: ({ get }) => {
-    const minute = get(minuteState);
-    return minute;
+  get: ({ get }) => get(minuteState),
+  set: ({ set }, newMinute) => set(minuteState, newMinute),
+});
+
+export const roundSelector = selector<IPoint>({
+  key: "roundSelector",
+  get: ({ get }) => get(roundPoint),
+  set: ({ set }, info) => {
+    set(roundPoint, info);
   },
-  set: ({ set }, newMinute) => {
-    set(minuteState, newMinute);
+});
+
+export const goalSelector = selector<IPoint>({
+  key: "goalSelector",
+  get: ({ get }) => get(goalPoint),
+  set: ({ set }, info) => {
+    set(goalPoint, info);
   },
 });
--- src/screens/Pomodoro.tsx
+++ src/screens/Pomodoro.tsx
@@ -2,7 +2,12 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
-import { hourSelector, minuteSelector } from "../atoms";
+import {
+  goalSelector,
+  hourSelector,
+  minuteSelector,
+  roundSelector,
+} from "../atoms";
 
 // styled + motion
 const Title = styled.h1`
@@ -72,7 +77,8 @@ const BtnWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 5% 5% auto;
+  margin-top: 5%;
+  margin-bottom: 10%;
 `;
 
 const Btn = styled(motion.button)`
@@ -86,6 +92,19 @@ const Btn = styled(motion.button)`
 const SvgButton = styled.svg`
   width: 50px;
   height: 50px;
+`;
+
+const BottomWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8%;
+
+  span {
+    font-size: 20px;
+    font-weight: 700;
+    color: rgba(255, 255, 255, 0.7);
+  }
 `;
 
 // animation -----
@@ -103,42 +122,56 @@ function Pomodoro() {
   const [playFlag, setPlayFlag] = useState(false);
   const [hour, setHour] = useRecoilState(hourSelector);
   const [minute, setMinute] = useRecoilState(minuteSelector);
+  const [roundInfo, setRoundInfo] = useRecoilState(roundSelector);
+  const [goalInfo, setGoalInfo] = useRecoilState(goalSelector);
 
   // 버튼을 클릭하면 play 아이콘이 바뀌어야 한다.
   // 그리고 1초 뒤에 59초가 되어야 한다.
-  const onChangePlayIcon = () => {
-    let flag = !playFlag;
-    setPlayFlag(flag);
+  const onChangePlayIcon = () => setPlayFlag((flag) => !flag);
 
-    // 0초일 경우엔 60 - 1 을 수행하도록 한다.
-    flag && Timer();
+  // 시, 분이 00:00 이면 roundPoint + 1가 기본적으로 계산된다.
+  // round가 4/4라면, 0/4로 되돌리고, goalPoint + 1를 계산한다.
+  const plusRoundAndGoal = () => {
+    if (!(hour === 0 && minute === 0)) return;
+
+    const { current, target } = roundInfo;
+
+    // 이 함수를 실행한 것 자체가 +1를 하기 위함.
+    setRoundInfo({ current: current + 1, target });
+
+    // 만약 round를 모두 채웠다면
+    if (current / target === 1) {
+      // 리셋시키고
+      setRoundInfo({ current: 0, target });
+      // goalPoint를 추가한다.
+      setGoalInfo({ current: goalInfo.current + 1, target: goalInfo.target });
+    }
   };
 
-  function Timer() {
-    setTimeout(() => {
-      // 분
-      minute !== 0 ? setMinute(minute - 1) : setMinute(59);
-      // 시
-      hour !== 0 && minute === 0 && setHour(hour - 1);
-    }, 1000);
-  }
-
-  // 최초 한번은 실행되는데, play가 되어야지만 수행되어야 한다.
+  // 타이머 기능: 분, 시 계산
   useEffect(() => {
+    // playFlag값이 true일때만 수행해야 한다.
     if (playFlag) {
-      // 1초동안 수행해야 함.
-      setTimeout(() => {
-        Timer();
+      const timer = setTimeout(() => {
+        // 분
+        minute !== 0 ? setMinute(minute - 1) : setMinute(3);
+        // 시
+        hour !== 0 && minute === 0 && setHour(hour - 1);
       }, 1000);
+
+      plusRoundAndGoal();
+
+      // 타이머 정리
+      return () => clearTimeout(timer);
     }
-  }, [minute]);
+  }, [playFlag, hour, minute, setHour, setMinute]);
 
   return (
     <>
       <Title>Pomodoro</Title>
       <Wrapper>
         <Hour>
-          <span>{hour}</span>
+          <span>{hour < 10 ? "0" + hour : hour}</span>
         </Hour>
         <Division>
           <span>:</span>
@@ -174,6 +207,15 @@ function Pomodoro() {
           )}
         </Btn>
       </BtnWrapper>
+
+      <BottomWrapper>
+        <span>
+          ROUND: {roundInfo.current} / {roundInfo.target} <br />
+        </span>
+        <span>
+          GOAL: {goalInfo.current} / {goalInfo.target}
+        </span>
+      </BottomWrapper>
     </>
   );
 }
